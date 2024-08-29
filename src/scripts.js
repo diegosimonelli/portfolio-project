0f6dc75126ea5e4583443b5ff3cdb9353d7eb75b
Diego Simonelli <dassimonelli@gmail.com>
2024-08-29 12:35:37 -0400

    dark mode fixed

--- src/scripts.js
+++ src/scripts.js
@@ -1,18 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.getElementById('theme-toggle');
+    const themeIcon = document.getElementById('theme-icon');
     const currentTheme = localStorage.getItem('theme') || 'day';
 
     document.documentElement.setAttribute('data-theme', currentTheme);
-    toggleButton.textContent = currentTheme === 'night' ? '🌜' : '🌞';
+
+    // Set the correct icon based on the current theme
+    themeIcon.src = currentTheme === 'night' ? 'night-mode.png' : 'light-mode.png';
 
     toggleButton.addEventListener('click', () => {
         let theme = document.documentElement.getAttribute('data-theme');
         if (theme === 'night') {
             theme = 'day';
-            toggleButton.textContent = '🌞';
+            themeIcon.src = 'light-mode.png';
         } else {
             theme = 'night';
-            toggleButton.textContent = '🌜';
+            themeIcon.src = 'night-mode.png';
         }
         document.documentElement.setAttribute('data-theme', theme);
         localStorage.setItem('theme', theme);
